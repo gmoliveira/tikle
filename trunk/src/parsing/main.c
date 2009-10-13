f65c5600813f8a61c711ca6a5061292ff6606d87
felipensp <felipensp@4ce9eb10-b6be-11de-8dfb-43c0dffa5306>
2009-10-13 23:25:01 +0000

    - License changes

--- trunk/src/parsing/main.c
+++ trunk/src/parsing/main.c
@@ -1,27 +1,24 @@
-/*
- * tikle kernel module
- * Copyright (c) 2009 Felipe 'ecl' Pena
- *                    Gustavo 'nst' Oliveira
+/* 
+ * Tikle kernel module
+ * Copyright (C) 2009  Felipe 'ecl' Pena
+ *                       Gustavo 'nst' Oliveira
+ * 
+ * Contact us at: #c2zlabs@freenode.net - www.c2zlabs.com
  *
- *   Contact us at:
- *   		   #c2zlabs@freenode.net
- *   		   www.c2zlabs.com
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *   This file is part of tikle.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   tikle is free software; you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- *   tikle is distributed in the hope that it will be useful, but
- *   WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with tikle; if not, write to the Free Software Foundation,
- *   Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also thanks to Higor 'enygmata' Euripedes
  */
 
 #include <sys/socket.h>
--- trunk/tikle_defs.h
+++ trunk/tikle_defs.h
@@ -1,3 +1,26 @@
+/* 
+ * Tikle kernel module
+ * Copyright (C) 2009  Felipe 'ecl' Pena
+ *                       Gustavo 'nst' Oliveira
+ * 
+ * Contact us at: #c2zlabs@freenode.net - www.c2zlabs.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Also thanks to Higor 'enygmata' Euripedes
+ */
+
 #ifndef TIKLE_DEFS_H
 #define TIKLE_DEFS_H
 
